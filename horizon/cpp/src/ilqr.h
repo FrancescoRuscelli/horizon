30aa02d10197d688e72e6d19b292be90a1e868e9
Arturo Laurenzi <arturo.laurenzi@gmail.com>
2021-08-04 18:36:13 +0200

    [ilqr] some docs

--- horizon/cpp/src/ilqr.h
+++ horizon/cpp/src/ilqr.h
@@ -17,21 +17,61 @@ typedef std::function<bool(const Eigen::MatrixXd& xtrj,
                            double defect_norm,
                            double constraint_violation)> CallbackType;
 
-
+/**
+ * @brief IterativeLQR implements a multiple-shooting variant of the
+ * notorious ILQR algorithm, implemented following the paper
+ * "A Family of Iterative Gauss-Newton Shooting Methods for Nonlinear
+ * Optimal Control" by M. Giftthaler, et al., from which most of the notation
+ * is taken.
+ *
+ * The framework supports arbitrary (differentiable) discrete time dynamics
+ * systems as well as arbitrary (twice differentiable) cost functions.
+ *
+ * Furthermore, arbitrary (differentiable) equality constraints are treated
+ * with a projection approach.
+ */
 class IterativeLQR
 {
 
 public:
 
+
+     /**
+     * @brief Class constructor
+     * @param fdyn is a function mapping state and control to the integrated state;
+     * required signature is (x, u) -> (f)
+     * @param N is the number of shooting intervals
+     */
     IterativeLQR(casadi::Function fdyn,
                  int N);
 
+    /**
+     * @brief set an intermediate cost term for each intermediate state
+     * @param inter_cost: a vector of N entries, each of which is a function with
+     * required signature (x, u) -> (l)
+     */
     void setIntermediateCost(const std::vector<casadi::Function>& inter_cost);
 
+    /**
+     * @brief set an intermediate cost term for the k-th intermediate state
+     * @param k: the node that the cost refers to
+     * @param inter_cost: a function with required signature (x, u) -> (l)
+     */
     void setIntermediateCost(int k, const casadi::Function& inter_cost);
 
+    /**
+     * @brief set the final cost
+     * @param final_cost: a function with required signature (x, u) -> (l),
+     * even though the input 'u' is not used
+     */
     void setFinalCost(const casadi::Function& final_cost);
 
+    /**
+     * @brief  set an intermediate constraint term for the k-th intermediate state
+     * @param k: the node that the cost refers to
+     * @param inter_constraint: a function with required signature (x, u) -> (h),
+     * where the constraint is h(x, u) = 0
+     */
     void setIntermediateConstraint(int k, const casadi::Function& inter_constraint);
 
     void setIntermediateConstraint(const std::vector<casadi::Function>& inter_constraint);
