af3ba8d399e700c8b59bb67f1ec4b7cf5c3abc0d
Arturo Laurenzi <arturo.laurenzi@gmail.com>
2021-08-05 17:44:58 +0200

    [bugfix] ilqr horizon integration

--- horizon/solvers/ilqr.py
+++ horizon/solvers/ilqr.py
@@ -1,34 +1,31 @@
+from horizon.solvers import solver
 from pyilqr import IterativeLQR
+from solver import Solver
 from horizon.problem import Problem
-from horizon.function import Function
-from typing import Dict
+from horizon.function import Function, CostFunction, Constraint
+from typing import Dict, List
 from horizon.utils import integrators
 import casadi as cs
 import numpy as np
 
-class SolverILQR:
+class SolverILQR(Solver):
     
     def __init__(self, 
                  prb: Problem, 
                  dt: float, 
                  opts: Dict = None) -> None:
 
-        # handle inputs
-        if opts is None:
-            opts = dict()
-        
-        self.prb = prb
+        # init base class
+        super().__init__(prb, dt, opts)
+
+        # save max iter if any
+        self.max_iter = self.opts.get('max_iter', 10)
         
         # num shooting interval
         self.N = prb.getNNodes() - 1  
 
-        # get state and control
-        self.x = prb.getState().getVars()
-        self.u = prb.getInput().getVars()
-        self.xdot = prb.getDynamics()
-
         # get integrator and compute discrete dynamics in the form (x, u) -> f
-        integrator_name = opts.get('ilqr.integrator', 'EULER')
+        integrator_name = self.opts.get('ilqr.integrator', 'EULER')
         dae = {'ode': self.xdot, 'x': self.x, 'p': self.u, 'quad': 0}
         self.int = integrators.__dict__[integrator_name](dae, {'tf': dt})
         self.dyn = cs.Function('f', {'x': self.x, 'u': self.u, 'f': self.int(self.x, self.u)[0]},
@@ -44,14 +41,15 @@ class SolverILQR:
 
     
     def solve(self):
+        self.ilqr.setInitialState(prb.getInitialState())
         self.ilqr.setIterationCallback(self._iter_callback)
-        self.ilqr.solve(5)
+        self.ilqr.solve(self.max_iter)
 
     def print_timings(self):
 
         prof_info = self.ilqr.getProfilingInfo()
         
-        print('\n\ntimings (inner):')
+        print('\ntimings (inner):')
         for k, v in prof_info.timings.items():
             if '_inner' not in k:
                 continue
@@ -68,20 +66,43 @@ class SolverILQR:
     
     def _set_cost_k(self, k):
         self._set_fun_k(k, 
-                self.prb.function_container.costfun_container, 
-                self.ilqr.setIntermediateCost, 
-                'l')
+                container=self.prb.function_container.costfun_container, 
+                set_to_ilqr=self.ilqr.setIntermediateCost, 
+                combine_elements=sum,
+                outname='l')
 
     
     def _set_constraint_k(self, k):
+        
+        def vertcat_list(l: List):
+            return cs.vertcat(*l)
+
         self._set_fun_k(k, 
-                self.prb.function_container.cnstr_container, 
-                self.ilqr.setIntermediateConstraint,
-                'h')
+                container=self.prb.function_container.cnstr_container, 
+                set_to_ilqr=self.ilqr.setIntermediateConstraint,
+                combine_elements=vertcat_list,
+                outname='h')
     
     
-    def _set_fun_k(self, k, container, set_to_ilqr, outname):
-                
+    def _set_fun_k(self, k, container, set_to_ilqr, combine_elements, outname):
+
+        value_list = list()
+
+        # check state and input bounds
+        if outname == 'h':
+
+            # state
+            xlb, xub = self.prb.getState().getBounds(node=k)
+            if np.all(xlb == xub):
+                value_list.append(self.x - xlb)
+
+            # input
+            if k < self.N:
+                ulb, uub = self.prb.getInput().getBounds(node=k)
+                if np.all(ulb == uub):
+                    value_list.append(self.u - ulb)
+
+        # check constraints    
         for fname, f in container.items():
             
             # give a type to f
@@ -100,12 +121,34 @@ class SolverILQR:
                 var: cs.SX = vlist[0]
                 input_list.append(var)
 
-            # wrap function
-            l = cs.Function(fname, [self.x, self.u], [f.fun(*input_list)[0]], 
+            # save function value to list
+            value = f.fun(*input_list)
+
+            # in the case of constraints, check bound values
+            if isinstance(f, Constraint):
+                lb, ub = Constraint.getBounds(f, nodes=k)
+                if np.any(lb != ub):
+                    raise ValueError(f'[ilqr] constraint {fname} not an equality constraint')
+                value -= lb
+
+            value_list.append(value)
+        
+        # if empty, skip
+        if len(value_list) == 0:
+            return 
+        
+        # compute overall value
+        total_value = combine_elements(value_list)
+        
+        print(f'{outname} at node {k:2}: {total_value}')
+
+        # wrap function
+        l = cs.Function(f'{outname}_{k}', [self.x, self.u], [total_value], 
                             ['x', 'u'], [outname])
 
-            # set it to solver
-            set_to_ilqr(k, l)
+        # set it to solver
+        set_to_ilqr(k, l)
+
 
     
     def _iter_callback(self, xtrj, utrj, du, cost, defect, constr):
@@ -123,6 +166,7 @@ class SolverILQR:
 
 
 ############# TESTING STUFF TO BE REMOVED #######################
+from matplotlib import pyplot as plt
 
 # create problem
 N = 100
@@ -144,13 +188,18 @@ xdot = cs.vertcat(v*cs.cos(theta),
 prb.setDynamics(xdot)
 
 # Cost function
-x_tgt = np.array([1.0, 0.0, 0.0])
+x_tgt = np.array([1, 0, 0])
 prb.createIntermediateCost("reg", 1e-6*cs.sumsqr(u))
 prb.createFinalConstraint("gothere", x - x_tgt)
 
+# initial state
+x0 = np.array([0, 0, np.pi/2])
+prb.setInitialState(x0=x0)
+
 # TEST ILQR
 sol = SolverILQR(prb, dt)
-x0 = np.array([0, 0, np.pi/2])
-sol.ilqr.setInitialState(x0)
 sol.solve()
 sol.print_timings()
+
+plt.plot(sol.ilqr.getStateTrajectory().T, '-')
+plt.show()
