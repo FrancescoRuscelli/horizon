195796b97bc663b8e9177fd5af31174be12979d7
Francesco Ruscelli <francesco.ruscelli@iit.it>
2021-08-10 09:58:13 +0200

    plotter updates

--- horizon/utils/plotter.py
+++ horizon/utils/plotter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib import gridspec
 from horizon.problem import Problem
+from horizon.state_variables import InputVariable
 import math
 import numpy as np
 import casadi as cs
@@ -26,14 +27,27 @@ class PlotterHorizon:
         return fig, gs
 
     def _plotVar(self, val, ax, abstract_var):
-        for dim in range(val.shape[0]):
-            baseline, = ax.plot(range(val.shape[1]), val[dim, :], marker="o")
-            lb, ub = abstract_var.getBounds()
-            ax.plot(range(val.shape[1]), lb[dim, :], marker="x", color=baseline.get_color())
-            ax.plot(range(val.shape[1]), ub[dim, :], marker="x", color=baseline.get_color())
+        baseline = None
+        if isinstance(abstract_var, InputVariable):
+            for dim in range(val.shape[0]):
+                for i in range(val.shape[1]-1):
+                    if baseline:
+                        baseline, = ax.plot(range(val.shape[1])[i:i+2], [val[dim, i]] * 2, color=baseline.get_color())
+                    else:
+                        baseline, = ax.plot(range(val.shape[1])[i:i + 2], [val[dim, i]] * 2)
+
+                    lb, ub = abstract_var.getBounds()
+                    ax.plot(range(val.shape[1]), lb[dim, :], marker="x", markersize=3, linestyle='dotted', linewidth=1, color=baseline.get_color())
+                    ax.plot(range(val.shape[1]), ub[dim, :], marker="x", markersize=3, linestyle='dotted', linewidth=1, color=baseline.get_color())
+        else:
+            for dim in range(val.shape[0]):
+                baseline, = ax.plot(range(val.shape[1]), val[dim, :], marker="o", markersize=2)
+                lb, ub = abstract_var.getBounds()
+                ax.plot(range(val.shape[1]), lb[dim, :], marker="x", markersize=3, linestyle='dotted', linewidth=1, color=baseline.get_color())
+                ax.plot(range(val.shape[1]), ub[dim, :], marker="x", markersize=3, linestyle='dotted', linewidth=1, color=baseline.get_color())
 
 
-    def plotVariables(self):
+    def plotVariables(self, grid=False):
 
         sol = self.prb.getSolution()
         if sol is None:
@@ -43,6 +57,8 @@ class PlotterHorizon:
         i = 0
         for key, val in sol.items():
             ax = fig.add_subplot(gs[i])
+            if grid:
+                ax.grid(axis='x')
             self._plotVar(val, ax, self.prb.getVariables(key))
 
             # options
@@ -56,7 +72,7 @@ class PlotterHorizon:
         fig.tight_layout()
         plt.show(block=False)
 
-    def plotVariable(self, name):
+    def plotVariable(self, name, grid=False):
 
         sol = self.prb.getSolution()
         if sol is None:
@@ -65,13 +81,15 @@ class PlotterHorizon:
         val = sol[name]
 
         fig, ax = plt.subplots()
+        if grid:
+            ax.grid(axis='x')
         self._plotVar(val, ax, prb.getVariables(name))
 
         ax.set_title('{}'.format(name))
         plt.xticks(list(range(val.shape[1])))
         ax.set(xlabel='nodes', ylabel='vals')
 
-    def plotFunctions(self):
+    def plotFunctions(self, grid=False):
 
         sol = self.prb.getSolution()
         if sol is None:
@@ -82,6 +100,8 @@ class PlotterHorizon:
         i = 0
         for name, fun in self.prb.getConstraints().items():
             ax = fig.add_subplot(gs[i])
+            if grid:
+                ax.grid(axis='x')
             fun_evaluated = self.prb.evalFun(fun)
             self._plotVar(fun_evaluated, ax, self.prb.getConstraints(name))
 
@@ -97,7 +117,7 @@ class PlotterHorizon:
 if __name__ == '__main__':
 
     nodes = 10
-    prb = Problem(nodes, logging_level=logging.DEBUG)
+    prb = Problem(nodes)
     x = prb.createStateVariable('x', 1)
     v = prb.createStateVariable('v', 1)
     k = prb.createVariable('k', 1, range(2, 8))
@@ -105,7 +125,10 @@ if __name__ == '__main__':
     t_tot = prb.createVariable('t', 1)
     t_tot.setBounds([100], [100])
 
-
+    u.setBounds(2, 2, 0)
+    u.setBounds(3, 3, 1)
+    u.setBounds(4, 4, 2)
+    u.setBounds(5, 5, 3)
     # danieli = prb.createConstraint('danieli', x-k, nodes=range(2, 8))
     diosporco = prb.createConstraint('diosporcomaledetto', x - t_tot)
     diosporco.setBounds([100], [100])
@@ -119,16 +142,13 @@ if __name__ == '__main__':
 
     prb.createProblem(opts=opts)
 
-    print(prb.getProblem()['x'])
-
     prb.solveProblem()
-
-
 
     hplt = PlotterHorizon(prb)
     hplt.plotVariables()
-    hplt.plotFunctions()
+    # hplt.plotFunctions()
 
+    plt.show()
     exit()
 
     nodes = 5
@@ -152,6 +172,8 @@ if __name__ == '__main__':
     hplt = PlotterHorizon(prb)
     hplt.plotVariables()
     hplt.plotFunctions()
+
+    plt.plot()
 
     exit()
 
@@ -181,4 +203,6 @@ if __name__ == '__main__':
     hplt = PlotterHorizon(sol)
 
     hplt.plotVariables()
+
+    plt.show()
     # hplt.plotVariable('x')
