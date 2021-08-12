d414da40e0b2bf3c11fdb6be93767d443a934ebf
EnricoMingo <enricomingo@gmail.com>
2021-08-12 15:03:57 +0200

    slighlty more complex test

--- horizon/cpp/src/sqp_test.cpp
+++ horizon/cpp/src/sqp_test.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-    auto x = casadi::SX::sym("x", 1);
+    auto x = casadi::SX::sym("x", 2);
     auto u = casadi::SX::sym("u", 1);
 
-    auto f = 3.*x*x + 2.*u;
+    auto f = 3.*x(0)*x(1)+ 2.*u;
     auto g = casadi::SX::vertcat({x, u});
 
     casadi::Dict opts;
@@ -14,14 +14,17 @@ int main()
     opts["printLevel"] = "none";
     horizon::SQPGaussNewton<casadi::SX> sqp("sqp", "qpoases", f, g, casadi::SX::vertcat({x, u}), opts);
 
-    casadi::DM x0(2,1);
+    casadi::DM x0(3,1);
     x0(0) = 2.;
     x0(1) = 3.;
+    x0(2) = 4.;
 
-    casadi::DM lb(2,1), ub(2,1);
+    casadi::DM lb(3,1), ub(3,1);
     lb(0) = 1.5;
     lb(1) = -1.;
+    lb(1) = -2.;
     ub(0) = 1.5;
+    ub(1) = 2.;
     ub(1) = 2.;
     auto solution = sqp.solve(x0, lb, ub, lb, ub);
 
