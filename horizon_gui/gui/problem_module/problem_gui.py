7fda427443fe7336d9c6aed0da5192923da6911b
Francesco Ruscelli <francesco.ruscelli@iit.it>
2021-09-08 00:02:41 +0200

    messing with spacer

--- horizon_gui/gui/problem_module/problem_gui.py
+++ horizon_gui/gui/problem_module/problem_gui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QLabel, QStackedWidget, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QRadioButton, QGridLayout, QPushButton, QSizePolicy
+from PyQt5.QtWidgets import QLabel, QStackedWidget, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QRadioButton, QGridLayout, QPushButton, QSizePolicy, QSpacerItem
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
 from horizon_gui.custom_widgets import on_destroy_signal_window, highlight_delegate, bounds_line, multi_slider, spinbox_line
@@ -26,10 +26,10 @@ class ProblemGui(QWidget):
         self.single_line_button.toggled.connect(partial(self.cost_line.switchPage, self.constraint_line.Single))
         self.multiple_line_button.toggled.connect(partial(self.constraint_line.switchPage, self.cost_line.Multi))
         self.multiple_line_button.toggled.connect(partial(self.cost_line.switchPage, self.cost_line.Multi))
-        
+
     def _initWidget(self):
 
-        self.main_layout = QGridLayout(self)
+        self.main_layout = QVBoxLayout(self)
         self.stacked_widget = QStackedWidget()
 
         self.constraint_page = QWidget()
@@ -38,7 +38,13 @@ class ProblemGui(QWidget):
         self.cost_page = QWidget()
         self.stacked_widget.addWidget(self.cost_page)
 
-        self.main_layout.addWidget(self.stacked_widget, 0, 0, 1, 2)
+        self.main_layout.addWidget(self.stacked_widget)
+
+        self.widget_buttons = QWidget()
+        self.widget_buttons_layer = QHBoxLayout(self.widget_buttons)
+
+        spacer_item = QSpacerItem(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.widget_buttons_layer.addItem(spacer_item)
 
         self.line_view = QWidget()
         self.line_view_layout = QHBoxLayout(self.line_view)
@@ -46,17 +52,22 @@ class ProblemGui(QWidget):
         self.line_view_layout.addWidget(self.label_view_title)
 
         self.multiple_line_button = QRadioButton()
+        self.multiple_line_button.setText('Single')
         self.multiple_line_button.setChecked(True)
         self.line_view_layout.addWidget(self.multiple_line_button)
 
         self.single_line_button = QRadioButton()
+        self.single_line_button.setText('Multiple')
         self.single_line_button.setChecked(False)
         self.line_view_layout.addWidget(self.single_line_button)
-
-        self.main_layout.addWidget(self.line_view, 2, 0, 1, 1)
+        self.widget_buttons_layer.addWidget(self.line_view)
 
         self.switch_page_button = QPushButton('Switch to Cost Functions')
-        self.main_layout.addWidget(self.switch_page_button, 2, 1, 1, 1)
+        self.widget_buttons_layer.addWidget(self.switch_page_button)
+
+        self.widget_buttons_layer.addStretch()
+        self.main_layout.addWidget(self.widget_buttons)
+
 
     def _initLines(self):
 
