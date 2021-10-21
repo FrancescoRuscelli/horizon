2abd15c9b4bf4f3bfe74d7c581c043fac1a8c89a
FrancescoRuscelli <francescoruscelli24@gmail.com>
2021-10-21 10:15:49 +0200

    opti_stuff

--- horizon/examples/opti_spot.py
+++ horizon/examples/opti_spot.py
@@ -1,4 +1,6 @@
 import casadi as cs
+import kiwisolver
+
 from horizon.utils import utils, kin_dyn, mat_storer
 import numpy as np
 import time
@@ -64,23 +66,20 @@ node_end_step = N
 # ====================
 # unraveled dimensions
 # ====================
-opti = cs.Opti()
-
-
 
 q_dim = N_states * n_q
 q_dot_dim = N_states * n_v
 q_ddot_dim = N_control * n_v
 f_dim = N_control * n_c * n_f
 
-q = opti.variable(n_q)
-q_dot = opti.variable(n_v)
-q_ddot = opti.variable(n_v)
+q = cs.SX.sym("q", n_q)
+q_dot = cs.SX.sym("q_dot", n_v)
+q_ddot = cs.SX.sym("q_ddot", n_v)
 
 # forces
 f_list = list()
 for i in range(n_c):
-    f_list.append(opti.variable(n_f))
+    f_list.append(cs.SX.sym(f"f{i}", n_f))
 
 # SET CONTACTS MAP
 contacts_name = {'lf_foot', 'rf_foot', 'lh_foot', 'rh_foot'}
@@ -103,6 +102,8 @@ integrator = RK4(dae, opts=dict(tf=dt))
 # integrator = integrator.expand()
 int_map = integrator.map(N_control, 'thread', 15)
 
+opti = cs.Opti()
+
 q_i = opti.variable(n_q, N_states)
 q_dot_i = opti.variable(n_v, N_states)
 
@@ -112,30 +113,12 @@ f_i = opti.variable(n_f * n_c, N_control)
 
 X = cs.vertcat(q_i, q_dot_i)
 U = cs.vertcat(q_ddot_i, f_i)
-exit()
 X_int = int_map(X[:, :N], U) # because it's N+1
 # starting from node 1
 g_multi_shoot = X_int[0] - X[:, 1:]
 
-
-q_init = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-                   0.0, 0.9, -1.5238505,
-                   0.0, 0.9, -1.5202315,
-                   0.0, 0.9, -1.5300265,
-                   0.0, 0.9, -1.5253125])
-
-# =====================================================================================
-# =====================================================================================
-#
 mu = 1
 R = np.identity(3, dtype=float)  # environment rotation wrt inertial frame
-
-g_v_list = list()
-g_lift_list = list()
-g_fc_list = list()
-g_no_force_list = list()
-# active_leg = slice(0,3)
-
 
 contact_map_i = dict(lf_foot=f_i[0:3, :],
                      rf_foot=f_i[3:6, :],
@@ -146,95 +129,70 @@ g_tau_i = kin_dyn.InverseDynamicsMap(N, kindyn,
                               contact_map.keys(),
                               cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED).call(q_i[:, :N], q_dot_i[:, :N], q_ddot_i, contact_map_i)
 
+
+tau_lim = np.array([0., 0., 0., 0., 0., 0.,  # Floating base
+                    1000., 1000., 1000.,  # Contact 1
+                    1000., 1000., 1000.,  # Contact 2
+                    1000., 1000., 1000.,  # Contact 3
+                    1000., 1000., 1000.])  # Contact 4
+
+opti.subject_to(opti.bounded(-tau_lim, g_tau_i, tau_lim))
 active_leg = ['lf_foot']
 
+opti.subject_to(g_multi_shoot == 0.)
+
 active_slice = slice(node_start_step, node_end_step)
+
+q_init = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                   0.0, 0.9, -1.5238505,
+                   0.0, 0.9, -1.5202315,
+                   0.0, 0.9, -1.5300265,
+                   0.0, 0.9, -1.5253125])
+
 i = 0
 for frame, f in contact_map.items():
     DFK = cs.Function.deserialize(kindyn.frameVelocity(frame, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED))
     v = DFK(q=q_i, qdot=q_dot_i)['ee_vel_linear']
     if frame in active_leg:
         # can move ee
-        g_v_list.append(v[:, 0:node_start_step])
-        # g_v_list.append(elem[85:])
+        opti.subject_to(v[:, 0:node_start_step] == 0)
     else:
         # 0 vel for ee
-        g_v_list.append(v)
+        opti.subject_to(v == 0)
+
 
     if frame in active_leg:
-        g_fc_list.append(kin_dyn.linearized_friction_cone_map(f_i[3 * i:3 * i + 3, 0:node_start_step], mu, R, node_start_step))
+        g_fc = kin_dyn.linearized_friction_cone_map(f_i[3 * i:3 * i + 3, 0:node_start_step], mu, R, node_start_step)
     else:
-        g_fc_list.append(kin_dyn.linearized_friction_cone_map(f_i[3 * i:3 * i + 3, :], mu, R, N_control))
+        g_fc = kin_dyn.linearized_friction_cone_map(f_i[3 * i:3 * i + 3, :], mu, R, N_control)
+
+    opti.subject_to(cs.vec(g_fc) <= 0)
+
 
     i += 1
-
     if frame in active_leg:
         FK = cs.Function.deserialize(kindyn.fk(frame))
         p_i = FK(q=q_i)['ee_pos']
         p_start = FK(q=q_init)['ee_pos']
         p_goal = p_start + [0., 0., 0.2]
 
-        g_lift_list.append(p_i[:, node_start_step + 20] - p_goal)
+        opti.subject_to(p_i[:, node_start_step + 20] - p_goal == 0)
 
 # no force during lift of one leg
-g_no_force_list.append(f_i[0:3, node_start_step:])
+opti.subject_to(f_i[0:3, node_start_step:] == 0)
 
+opti.minimize(1e-2 * cs.sumsqr(f_i[0:3, :]))
+opti.minimize(1e-2 * cs.sumsqr(f_i[3:6, :]))
+opti.minimize(1e-2 * cs.sumsqr(f_i[6:9, :]))
+opti.minimize(1e-2 * cs.sumsqr(f_i[9:12, :]))
 
-# g_land = p_i[:, 90] - p_start
-# prb.createConstraint(f"land_{frame}_leg", p - p_start, nodes=node_end_step + 2)
-
-# prb.createConstraint(f"lift_{frame}_leg", p - p_goal, nodes=25)
-# prb.createConstraint(f"land_{frame}_leg", p - p_start, nodes=node_end_step + 2)
-
-f_list = list()
-# f_list.append(1000 * cs.sumsqr(q_i - q_init))
-# f_list.append(1e-4 * cs.sumsqr(f_i))
-f_list.append(1e-2 * cs.sumsqr(f_i[0:3, :]))
-f_list.append(1e-2 * cs.sumsqr(f_i[3:6, :]))
-f_list.append(1e-2 * cs.sumsqr(f_i[6:9, :]))
-f_list.append(1e-2 * cs.sumsqr(f_i[9:12, :]))
-
-f_list.append(1e3 * cs.sumsqr(q_dot_i))
-# f_list.append(1 * cs.sumsqr(q_ddot_i))
-# =====================================================================================
-# =====================================================================================
-# f_list.append(cs.sumsqr(q_base))
-# ====================
-# setting initial condition
-# ====================
-q0 = np.zeros([n_q, N_states])
-q_dot0 = np.zeros([n_v, N_states])
-q_ddot0 = np.zeros([n_v, N_control])
-f0 = np.zeros([n_c * n_f, N_control])
-
-# ====================
-# lower bound variables
-# ====================
-q_lbw = -np.inf * np.ones([n_q, N_states])
-q_dot_lbw = -np.inf * np.ones([n_v, N_states])
-q_ddot_lbw = -np.inf * np.ones([n_v, N_control])
-f_lbw = -np.inf * np.ones([n_c * n_f, N_control])
-
-# ====================
-# upper bound variables
-# ====================
-q_ubw = np.inf * np.ones([n_q, N_states])
-q_dot_ubw = np.inf * np.ones([n_v, N_states])
-q_ddot_ubw = np.inf * np.ones([n_v, N_control])
-f_ubw = np.inf * np.ones([n_c * n_f, N_control])
+opti.minimize(1e3 * cs.sumsqr(q_dot_i))
 
 # ===============================
 # ==== BOUNDS INITIALIZATION ====
 # ===============================
 # initial guess q
-q0[:, 0] = q_init
-
-q_dot_lbw[:, 0] = np.zeros([1, n_v])
-q_dot_ubw[:, 0] = np.zeros([1, n_v])
-
-# zero final velocity
-q_dot_lbw[:, N] = np.zeros([1, n_v])
-q_dot_ubw[:, N] = np.zeros([1, n_v])
+opti.set_initial(q_i[:, 0], q_init)
 
 q_min = [-10., -10., -10., -1., -1., -1., -1.]  # floating base
 q_min.extend(kindyn.q_min()[7:])
@@ -244,177 +202,54 @@ q_max = [10., 10., 10., 1., 1., 1., 1.]  # floating base
 q_max.extend(kindyn.q_max()[7:])
 q_max = np.array(q_max)
 
-q_lbw[:, :] = np.tile(q_min, (N_states, 1)).T
-q_ubw[:, :] = np.tile(q_max, (N_states, 1)).T
-
-# initial q
-q_lbw[:, 0] = q_init
-q_ubw[:, 0] = q_init
+opti.subject_to(q_i[:, 0] == q_init)
+opti.subject_to(opti.bounded(q_min, q_i, q_max))
 
 # q_dot bounds
 q_dot_lim = 100. * np.ones(n_v)
-q_dot_lbw[:, :] = np.tile(-q_dot_lim, (N_states, 1)).T
-q_dot_ubw[:, :] = np.tile(q_dot_lim, (N_states, 1)).T
+opti.subject_to(opti.bounded(-q_dot_lim, q_dot_i, q_dot_lim))
+opti.subject_to(q_dot_i[:, 0] == 0)
+opti.subject_to(q_dot_i[:, N] == 0)
 
 # q_ddot bounds
 q_ddot_lim = 100. * np.ones(n_v)
-q_ddot_lbw[:, :] = np.tile(-q_ddot_lim, (N_control, 1)).T
-q_ddot_ubw[:, :] = np.tile(q_ddot_lim, (N_control, 1)).T
+opti.subject_to(opti.bounded(-q_ddot_lim, q_ddot_i, q_ddot_lim))
+
 
 # f bounds
 f_lim = 1000. * np.ones(n_c * n_f)
-f_lbw[:, :] = np.tile(-f_lim, (N_control, 1)).T
-f_ubw[:, :] = np.tile(f_lim, (N_control, 1)).T
+opti.subject_to(opti.bounded(-f_lim, f_i, f_lim))
+opti.subject_to(opti.bounded(0, f_i[2, :], 1000))
+opti.subject_to(opti.bounded(0, f_i[5, :], 1000))
+opti.subject_to(opti.bounded(0, f_i[8, :], 1000))
+opti.subject_to(opti.bounded(0, f_i[11, :], 1000))
 
-#
-f_lbw[2, :] = np.zeros(f_lbw[2, :].shape)
-f_lbw[5, :] = np.zeros(f_lbw[5, :].shape)
-f_lbw[8, :] = np.zeros(f_lbw[8, :].shape)
-f_lbw[11, :] = np.zeros(f_lbw[11, :].shape)
-
-# lower bound constraints
-tau_lim = np.array([0., 0., 0., 0., 0., 0.,  # Floating base
-                    1000., 1000., 1000.,  # Contact 1
-                    1000., 1000., 1000.,  # Contact 2
-                    1000., 1000., 1000.,  # Contact 3
-                    1000., 1000., 1000.])  # Contact 4
-
-tau_g_lbw = np.tile(-tau_lim, (N_control, 1)).T
-multi_shoot_g_lbw = np.zeros(g_multi_shoot.shape)
-
-g_lift_lbw = list()
-for elem in g_lift_list:
-    g_lift_lbw.append(np.zeros(elem.shape))
-
-g_lift_ubw = list()
-for elem in g_lift_list:
-    g_lift_ubw.append(np.zeros(elem.shape))
-
-g_v_lbw = list()
-for elem in g_v_list:
-    g_v_lbw.append(np.zeros(elem.shape))
-
-g_v_ubw = list()
-for elem in g_v_list:
-    g_v_ubw.append(np.zeros(elem.shape))
-
-g_fc_lbw = list()
-for elem in g_fc_list:
-    g_fc_lbw.append(-np.inf * np.ones(elem.shape))
-
-g_fc_ubw = list()
-for elem in g_fc_list:
-    g_fc_ubw.append(np.zeros(elem.shape))
-
-g_no_force_lbw = list()
-for elem in g_no_force_list:
-    g_no_force_lbw.append(np.zeros(elem.shape))
-
-g_no_force_ubw = list()
-for elem in g_no_force_list:
-    g_no_force_ubw.append(np.zeros(elem.shape))
-
-# upper bound constraints
-tau_g_ubw = np.tile(tau_lim, (N_control, 1)).T
-multi_shoot_g_ubw = np.zeros(g_multi_shoot.shape)
-# g_land_ubw = np.zeros(g_land.shape)
-
-# ======================================================================================================================
-# reshape stuff ========================================================================================================
-# ======================================================================================================================
-
-q0_flat = np.reshape(q0, [1, q_dim], order='F')
-q_dot0_flat = np.reshape(q_dot0, [1, q_dot_dim], order='F')
-q_ddot0_flat = np.reshape(q_ddot0, [1, q_ddot_dim], order='F')
-f0_flat = np.reshape(f0, [1, f_dim], order='F')
-w0 = np.concatenate((q0_flat, q_dot0_flat, q_ddot0_flat, f0_flat), axis=1)
-
-q_lbw_flat = np.reshape(q_lbw, [1, q_dim], order='F')
-q_dot_lbw_flat = np.reshape(q_dot_lbw, [1, q_dot_dim], order='F')
-q_ddot_lbw_flat = np.reshape(q_ddot_lbw, [1, q_ddot_dim], order='F')
-f_lbw_flat = np.reshape(f_lbw, [1, f_dim], order='F')
-lbw = np.concatenate((q_lbw_flat, q_dot_lbw_flat, q_ddot_lbw_flat, f_lbw_flat), axis=1)
-
-q_ubw_flat = np.reshape(q_ubw, [1, q_dim], order='F')
-q_dot_ubw_flat = np.reshape(q_dot_ubw, [1, q_dot_dim], order='F')
-q_ddot_ubw_flat = np.reshape(q_ddot_ubw, [1, q_ddot_dim], order='F')
-f_ubw_flat = np.reshape(f_ubw, [1, f_dim], order='F')
-ubw = np.concatenate((q_ubw_flat, q_dot_ubw_flat, q_ddot_ubw_flat, f_ubw_flat), axis=1)
-
-tau_g_lbw = np.reshape(tau_g_lbw, [1, g_tau_i.shape[0] * g_tau_i.shape[1]], order='F')
-tau_g_ubw = np.reshape(tau_g_ubw, [1, g_tau_i.shape[0] * g_tau_i.shape[1]], order='F')
-
-multi_shoot_g_lbw_flat = np.reshape(multi_shoot_g_lbw, [1, g_multi_shoot.shape[0] * g_multi_shoot.shape[1]], order='F')
-multi_shoot_g_ubw_flat = np.reshape(multi_shoot_g_ubw, [1, g_multi_shoot.shape[0] * g_multi_shoot.shape[1]], order='F')
-
-g_lift_lbw_flat = list()
-for elem in g_lift_lbw:
-    g_lift_lbw_flat = np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F')
-
-g_lift_ubw_flat = list()
-for elem in g_lift_ubw:
-    g_lift_ubw_flat = np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F')
-
-g_v_lbw_flat = list()
-for elem in g_v_lbw:
-    g_v_lbw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-g_v_ubw_flat = list()
-for elem in g_v_ubw:
-    g_v_ubw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-g_fc_lbw_flat = list()
-for elem in g_fc_lbw:
-    g_fc_lbw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-g_fc_ubw_flat = list()
-for elem in g_fc_ubw:
-    g_fc_ubw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-g_no_force_lbw_flat = list()
-for elem in g_no_force_lbw:
-    g_no_force_lbw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-g_no_force_ubw_flat = list()
-for elem in g_no_force_ubw:
-    g_no_force_ubw_flat.append(np.reshape(elem, [1, elem.shape[0] * elem.shape[1]], order='F'))
-
-
-# ======================================================================================================================
-# ======================================================================================================================
-# ======================================================================================================================
-w = cs.veccat(q_i, q_dot_i, q_ddot_i, f_i)
-g = cs.veccat(g_multi_shoot, g_tau_i, *g_v_list, *g_no_force_list, *g_lift_list, *g_fc_list) # *g_no_force_list # g_land
-f = sum(f_list)
-
-
-lbg = np.concatenate((multi_shoot_g_lbw_flat, tau_g_lbw, *g_v_lbw_flat, *g_no_force_lbw_flat, g_lift_lbw_flat, *g_fc_lbw_flat), axis=1)  #*g_no_force_lbw_flat,  g_land_lbw_flat
-ubg = np.concatenate((multi_shoot_g_ubw_flat, tau_g_ubw, *g_v_ubw_flat, *g_no_force_ubw_flat, g_lift_ubw_flat, *g_fc_ubw_flat), axis=1) #*g_no_force_ubw_flat,  g_land_ubw_flat
-
-prob_dict = {'f': f, 'x': w, 'g': cs.vec(g)}
-
-opts = {
-        'ipopt.tol': 0.001,
-        'ipopt.constr_viol_tol': 0.001,
-        'ipopt.max_iter': 2000,
-        'ipopt.linear_solver': 'ma57'}
+s_opts = {
+           'ipopt.tol': 0.001,
+           'ipopt.constr_viol_tol': 0.001,
+           'ipopt.max_iter': 2000,
+           'ipopt.linear_solver': 'ma57'}
         # 'verbose': True,
         # banned options:
         # 'ipopt.hessian_approximation': 'limited-memory',
         # 'expand': True,
 
-# create solver from prob
-tic = time.time()
-solver = cs.nlpsol('solver', 'ipopt', prob_dict, opts)
-toc = time.time()
-print('time elapsed loading:', toc - tic)
 
-# print(ubw)
-# exit()
+opti.solver('ipopt', s_opts)
+
 tic = time.time()
-solution = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg)
+sol = opti.solve()
 toc = time.time()
 print('time elapsed solving:', toc - tic)
 
+
+solution = dict()
+solution['q_i'] = sol.value(q_i)
+solution['q_dot_i'] = sol.value(q_dot_i)
+solution['q_ddot_i'] = sol.value(q_ddot_i)
+solution['f_i'] = sol.value(f_i)
+
+
 ms = mat_storer.matStorer(f'{os.path.splitext(os.path.basename(__file__))[0]}.mat')
-ms.store(dict(a=np.array(solution['x'])))
+
+ms.store(dict(a=solution))
---
+++ horizon/utils/replayer_opti_stack.py
@@ -0,0 +1,153 @@
+import time
+
+import numpy as np
+
+from horizon import problem
+from horizon.utils import utils, kin_dyn, resampler_trajectory, plotter, mat_storer
+from horizon.transcriptions import integrators
+from horizon.transcriptions.transcriptor import Transcriptor
+from horizon.ros.replay_trajectory import *
+from horizon.solvers import solver
+import matplotlib.pyplot as plt
+import os
+from scipy.io import loadmat
+
+
+urdffile = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../examples/urdf', 'spot.urdf')
+urdf = open(urdffile, 'r').read()
+kindyn = cas_kin_dyn.CasadiKinDyn(urdf)
+
+n_c = 4
+n_q = kindyn.nq()
+n_v = kindyn.nv()
+n_f = 3
+
+N = 100
+N_states = N + 1
+N_control = N
+
+dt = 0.01
+
+ms = mat_storer.matStorer('../examples/opti_spot.mat')
+solution = ms.load()
+
+sol = solution['a']
+i = 0
+q_dim = N_states * n_q
+q_dot_dim = N_states * n_v
+q_ddot_dim = N_control * n_v
+f_dim = N_control * n_c * n_f
+
+q = sol['q_i'][0][0]
+q_dot = sol['q_dot_i'][0][0]
+q_ddot = sol['q_ddot_i'][0][0]
+f = sol['f_i'][0][0]
+
+
+f1 = f[0 * n_f:1 * n_f, :]
+f2 = f[1 * n_f:2 * n_f, :]
+f3 = f[2 * n_f:3 * n_f, :]
+f4 = f[3 * n_f:4 * n_f, :]
+
+f_list = [f1, f2, f3, f4]
+
+# ========
+# replaying
+# ========
+replay_traj = False
+if replay_traj:
+    joint_names = kindyn.joint_names()
+    if 'universe' in joint_names: joint_names.remove('universe')
+    if 'floating_base_joint' in joint_names: joint_names.remove('floating_base_joint')
+    repl = replay_trajectory(dt, joint_names, q, None, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED, kindyn)
+
+    repl.sleep(1.)
+    repl.replay(is_floating_base=True)
+# ========
+# plotting
+# ========
+contacts_name = {'lf_foot', 'rf_foot', 'lh_foot', 'rh_foot'}
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+for dim in range(q.shape[0]):
+    plt.plot(range(q.shape[1]), np.array(q[dim, :]))
+plt.title('q')
+
+plt.figure()
+for dim in range(q_dot.shape[0]):
+    plt.plot(range(q_dot.shape[1]), np.array(q_dot[dim, :]))
+plt.title('q_dot')
+
+plt.figure()
+for dim in range(q_ddot.shape[0]):
+    plt.plot(range(q_ddot.shape[1]), np.array(q_ddot[dim, :]))
+plt.title('q_ddot')
+
+pos_contact_list = list()
+for contact in contacts_name:
+    FK = cs.Function.deserialize(kindyn.fk(contact))
+    pos = FK(q=q)['ee_pos']
+    plt.figure()
+    plt.title(contact)
+    for dim in range(n_f):
+        plt.plot(np.array([range(pos.shape[1])]), np.array(pos[dim, :]), marker="x", markersize=3, linestyle='dotted')
+
+    plt.vlines([30, N+1], plt.gca().get_ylim()[0], plt.gca().get_ylim()[1], linestyles='dashed', colors='k', linewidth=0.4)
+
+# plane_xy
+plt.figure()
+
+for contact in contacts_name:
+    FK = cs.Function.deserialize(kindyn.fk(contact))
+    pos = FK(q=q)['ee_pos']
+
+    plt.title(f'plane_xy')
+    plt.scatter(np.array(pos[0, :]), np.array(pos[1, :]))
+    plt.scatter(np.array(pos[0, 0]), np.array(pos[1, 0]), c='blue')
+    plt.scatter(np.array(pos[0, -1]), np.array(pos[1, -1]), c='green')
+
+FK = cs.Function.deserialize(kindyn.centerOfMass())
+pos_com = FK(q=q)['com']
+plt.scatter(np.array(pos_com[0, :]), np.array(pos_com[1, :]), marker='x')
+plt.scatter(np.array(pos_com[0, 0]), np.array(pos_com[1, 0]), marker='x', c='blue')
+plt.scatter(np.array(pos_com[0, -1]), np.array(pos_com[1, -1]), marker='x', c='green')
+
+# plane_xz
+plt.figure()
+for contact in contacts_name:
+    FK = cs.Function.deserialize(kindyn.fk(contact))
+    pos = FK(q=q)['ee_pos']
+
+    plt.title(f'plane_xz')
+    plt.scatter(np.array(pos[0, :]), np.array(pos[2, :]), linewidth=0.1)
+    plt.scatter(np.array(pos[0, 0]), np.array(pos[2, 0]),  c='blue')
+    plt.scatter(np.array(pos[0, -1]), np.array(pos[2, -1]), c='green')
+
+FK = cs.Function.deserialize(kindyn.centerOfMass())
+pos_com = FK(q=q)['com']
+plt.scatter(np.array(pos_com[1, :]), np.array(pos_com[2, :]), marker='x')
+plt.scatter(np.array(pos_com[0, 0]), np.array(pos_com[2, 0]), marker='x', c='blue')
+plt.scatter(np.array(pos_com[0, -1]), np.array(pos_com[2, -1]), marker='x', c='green')
+
+plt.figure()
+plt.scatter(np.array(pos_com[1, :]), np.array(pos_com[2, :]), marker='x')
+# forces
+plt.figure()
+
+for f in f_list:
+    plt.plot(np.array(range(f.shape[1])), f[0, :])
+    plt.title(f'forces_x')
+
+plt.figure()
+for f in f_list:
+    plt.plot(np.array(range(f.shape[1])), f[1, :])
+    plt.title(f'forces_y')
+
+plt.figure()
+for f in f_list:
+    plt.plot(np.array(range(f.shape[1])), f[2, :])
+    plt.title(f'forces_z')
+
+plt.show()
