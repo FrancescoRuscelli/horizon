1159393903a6a154732bc1be59de6975d708fa9e
enricomingo <enrico.mingo@pal-robotics.com>
2021-11-02 15:31:43 +0100

    Added possibility to store more data than only the solution in mat_storer

--- horizon/utils/mat_storer.py
+++ horizon/utils/mat_storer.py
@@ -4,13 +4,21 @@ import numpy as np
 class matStorer:
     def __init__(self, file_name):
         self.file_name = file_name
+        self.dict_values = dict()
 
     # def append(self, values):
     #     with open(self.file_name, 'ab') as f:
     #         savemat(f, {'pad': values})
 
+    def append(self, dict_values):
+        self.dict_values.update(dict_values)
+
     def store(self, dict_values):
-        savemat(self.file_name, dict_values)  # write
+        self.append(dict_values)
+        savemat(self.file_name, self.dict_values)  # write
+
+    def save(self):
+        savemat(self.file_name, self.dict_values)
 
     def load(self):
         return loadmat(self.file_name)
