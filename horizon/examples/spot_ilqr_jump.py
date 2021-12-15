533796839b4eae06d080f399efb4edfc865ac0cb
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-12-15 18:38:09 +0100

    cleaned spot_ilqr_jump

--- horizon/examples/spot_ilqr_jump.py
+++ horizon/examples/spot_ilqr_jump.py
@@ -1,5 +1,3 @@
-import time
-
 from horizon import problem
 from horizon.utils import utils, kin_dyn, resampler_trajectory, plotter, mat_storer
 from horizon.transcriptions import integrators
@@ -7,23 +5,36 @@ from horizon.transcriptions.transcriptor import Transcriptor
 from horizon.ros.replay_trajectory import *
 from horizon.solvers import solver
 import matplotlib.pyplot as plt
+import time
 import os, math
 from itertools import filterfalse
+import argparse
 
-# mat storer
+# command line arguments
+parser = argparse.ArgumentParser('Horizon\'s twist jump motion with BostonDynamics SpotMini robot.')
+parser.add_argument('--solver', '-s', default='ilqr', help='Solver type (ilqr, gnsqp, ipopt)')
+parser.add_argument('--codegen', '-c', action='store_true', help='Code-generate problem prior to solution (only ilqr for now)')
+parser.add_argument('--verbose', '-v', action='store_true', help='Print more output')
+parser.add_argument('--interactive', '-i', action='store_true', help='Plot each inner iteration of the solver (only ilqr for now)')
+parser.add_argument('--resample', '-r', action='store_true', help='Resample solution prior to replaying it')
+
+args = parser.parse_args()
+parser.print_help()
+
+# output mat file 
 filename_with_ext = __file__
 filename, _ = os.path.splitext(filename_with_ext)
-ms = mat_storer.matStorer(f'{filename}.mat')
+basename = os.path.basename(filename)
+ms = mat_storer.matStorer(f'/tmp/{basename}.mat')
+print(f'will save solution to /tmp/{basename}.mat')
 
 # options
-solver_type = 'ilqr'
-
+solver_type = args.solver
 transcription_method = 'multiple_shooting'
 transcription_opts = dict(integrator='RK4')
-load_initial_guess = False
 tf = 2.0
 n_nodes = 100
-ilqr_plot_iter = False
+ilqr_plot_iter = args.interactive
 t_jump = (1.0, 1.5)
 
 # load urdf
@@ -59,30 +70,30 @@ contact_map = dict(zip(contacts_name, f_list))
 
 # initial state and initial guess
 q_init = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-                   0.0, 0.9, -1.5238505,
-                   0.0, 0.9, -1.5202315,
-                   0.0, 0.9, -1.5300265,
-                   0.0, 0.9, -1.5253125])
+                   0.0, 0.9, -1.52,
+                   0.0, 0.9, -1.52,
+                   0.0, 0.9, -1.52,
+                   0.0, 0.9, -1.52])
 
 q.setBounds(q_init, q_init, 0)
 q_dot.setBounds(np.zeros(n_v), np.zeros(n_v), 0)
-
-
-
 q.setInitialGuess(q_init)
 
 for f in f_list:
     f.setInitialGuess([0, 0, 55])
 
-# transcription
+# transcription (if not ilqr)
 if solver_type != 'ilqr':
     th = Transcriptor.make_method(transcription_method, prb, opts=transcription_opts)
 
 # dynamic feasibility
-id_fn = kin_dyn.InverseDynamics(kindyn, contact_map.keys(), cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED)
+pin_ref_frame = cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED
+id_fn = kin_dyn.InverseDynamics(kindyn, contact_map.keys(), pin_ref_frame)
 tau = id_fn.call(q, q_dot, q_ddot, contact_map)
 prb.createIntermediateConstraint("dynamic_feasibility", tau[:6])
 
+# gauss-newton sqp works with residuals!
+# tbd: check this under the hood
 if solver_type == 'gnsqp':
     def residual_to_cost(r):
         return r
@@ -96,21 +107,15 @@ prb.createFinalConstraint('final_velocity', q_dot)
 # final pose
 q_tgt = q_init.copy()
 q_tgt[0] = 0
-q_tgt[5] = math.sin(math.pi/4)
+q_tgt[5] = math.sin(math.pi/4)  # twist jump 90 deg!
 prb.createFinalConstraint('q_fb', q[:6] - q_tgt[:6])
-# prb.createFinalConstraint('q_f', q[7:] - q_tgt[7:])
-
 prb.createFinalCost('q_f', residual_to_cost(10*(q[7:] - q_tgt[7:])))
-# prb.createFinalCost('q_dot_f', 100*cs.sumsqr(q_dot))
 
 # contact handling
 k_all = range(1, n_nodes+1)
 k_swing = list(range(*[int(t/dt) for t in t_jump]))
 k_stance = list(filterfalse(lambda k: k in k_swing, k_all))
 lifted_legs = contacts_name.copy()
-
-def barrier(x):
-    return cs.if_else(x > 0, 0, x**2)
 
 # contact velocity is zero, and normal force is positive
 for frame, f in contact_map.items():
@@ -127,29 +132,24 @@ for frame, f in contact_map.items():
 
     # node: on first swing node vel should be zero!
     prb.createConstraint(f"{frame}_vel", v, nodes=list(nodes) + [k_swing[0]])
-    # prb.createIntermediateCost(f'{frame}_fn', barrier(f[2] - 25.0)) #, nodes=nodes)
+
 
 # swing force is zero
 for leg in lifted_legs:
     fzero = np.zeros(n_f)
     contact_map[leg].setBounds(fzero, fzero, nodes=k_swing)
 
-
 # cost
-# prb.createCost("min_rot", 10 * cs.sumsqr(q[3:6] - q_init[3:6]))
-# prb.createCost("min_xy", 100 * cs.sumsqr(q[0:2] - q_init[0:2]))
 prb.createCost("min_q", residual_to_cost(1e-1 * (q[7:] - q_init[7:])))
-# prb.createCost("min_q_dot", 1e-2 * cs.sumsqr(q_dot))
 prb.createIntermediateCost("min_q_ddot", residual_to_cost(1e-3* (q_ddot)))
 for f in f_list:
     prb.createIntermediateCost(f"min_{f.getName()}", residual_to_cost(1e-3 * (f)))
 
 
-
-
 # =============
 # SOLVE PROBLEM
 # =============
+
 opts = dict()
 if solver_type == 'ipopt':
     opts['ipopt.tol'] = 0.001
@@ -158,16 +158,15 @@ if solver_type == 'ipopt':
     opts['ipopt.linear_solver'] = 'ma57'
 
 if solver_type == 'ilqr':
-    opts = {'ilqr.max_iter': 1000,
-        'ilqr.integrator': 'RK4', 
-        'ilqr.closed_loop_forward_pass': True,
-        'ilqr.line_search_accept_ratio': 1e-9,
-        'ilqr.svd_threshold': 1e-12,
-        'ilqr.kkt_decomp_type': 'lu',
-        'ilqr.constr_decomp_type': 'qr',
-        'ilqr.codegen_enabled': False,
-        'ilqr.codegen_workdir': '/tmp/ilqr_spot_jump',
-        }
+    opts = {
+            'ilqr.max_iter': 1000,
+            'ilqr.integrator': 'RK4', 
+            'ilqr.svd_threshold': 1e-12,
+            'ilqr.kkt_decomp_type': 'ldlt',
+            'ilqr.constr_decomp_type': 'qr',
+            'ilqr.codegen_enabled': args.codegen,
+            'ilqr.codegen_workdir': f'/tmp/{basename}',
+            }
 
 if solver_type == 'gnsqp':
     qp_solver = 'osqp'
@@ -178,29 +177,15 @@ if solver_type == 'gnsqp':
         opts['merit_derivative_tolerance'] = 1e-6
         opts['constraint_violation_tolerance'] = n_nodes * 1e-12
         opts['osqp.polish'] = True # without this
-        opts['osqp.delta'] = 1e-9 # and this, it does not converge!
+        opts['osqp.delta'] = 1e-9  # and this, it does not converge!
         opts['osqp.verbose'] = False
         opts['osqp.rho'] = 0.02
         opts['osqp.scaled_termination'] = False
-    if qp_solver == 'qpoases': #does not work!
-        opts['gnsqp.qp_solver'] = 'qpoases'
-        opts['sparse'] = True
-        opts["enableEqualities"] = True
-        opts["initialStatusBounds"] = "inactive"
-        opts["numRefinementSteps"] = 0
-        opts["enableDriftCorrection"] = 0
-        opts["terminationTolerance"] = 10e9 * 1e-7
-        opts["enableFlippingBounds"] = False
-        opts["enableNZCTests"] = False
-        opts["enableRamping"] = False
-        opts["enableRegularisation"] = True
-        opts["numRegularisationSteps"] = 2
-        opts["epsRegularisation"] = 5. * 10e3 * 1e-7
-        opts['hessian_type'] =  'posdef'
-        #opts['printLevel'] = 'high'
-        opts['linsol_plugin'] = 'ma57'
-
+  
+t = time.time()
 solver = solver.Solver.make_solver(solver_type, prb, opts)
+elapsed = time.time() - t
+print(f'built problem in {elapsed} s')
 
 try:
     solver.set_iteration_callback()
@@ -212,12 +197,6 @@ t = time.time()
 solver.solve()
 elapsed = time.time() - t
 print(f'solved in {elapsed} s')
-
-if solver_type == 'gnsqp':
-    print(f"mean Hessian computation time: {sum(solver.getHessianComputationTime())/len(solver.getHessianComputationTime())}")
-    print(f"mean QP computation time: {sum(solver.getQPComputationTime())/len(solver.getQPComputationTime())}")
-    print(f"mean Line Search computation time: {sum(solver.getLineSearchComputationTime()) / len(solver.getLineSearchComputationTime())}")
-
 
 try:
     solver.print_timings()
@@ -234,64 +213,9 @@ else:
     ms.store({**solution, **solution_constraints_dict, **dt_dict})
 
 
-# ========================================================
-plot_all = False
-plot_fun = False
-plot_forces = False
-
-if plot_forces:
-    for f in [f'f{i}' for i in range(len(contacts_name))]:
-        plt.figure()
-        for dim in range(solution[f].shape[0]):
-            plt.plot(np.array(range(solution[f].shape[1])), solution[f][dim, :])
-
-        plt.title(f'force {f}')
-
-    plt.show()
-
-if plot_fun:
-
-    hplt = plotter.PlotterHorizon(prb, solution)
-    # hplt.plotVariables(show_bounds=True, legend=False)
-    hplt.plotFunctions(show_bounds=True)
-    # hplt.plotFunction('inverse_dynamics', show_bounds=True, legend=True, dim=range(6))
-    plt.show()
-
-if plot_all:
-    pos_contact_list = list()
-    for contact in contacts_name:
-        FK = cs.Function.deserialize(kindyn.fk(contact))
-        pos = FK(q=solution['q'])['ee_pos']
-        plt.figure()
-        plt.title(contact)
-        for dim in range(n_f):
-            plt.plot(np.array([range(pos.shape[1])]), np.array(pos[dim, :]), marker="x", markersize=3, linestyle='dotted')
-
-        plt.vlines([node_start_step, node_end_step], plt.gca().get_ylim()[0], plt.gca().get_ylim()[1], linestyles='dashed', colors='k', linewidth=0.4)
-
-    plt.figure()
-    for contact in contacts_name:
-        FK = cs.Function.deserialize(kindyn.fk(contact))
-        pos = FK(q=solution['q'])['ee_pos']
-
-        plt.title(f'plane_xy')
-        plt.scatter(np.array(pos[0, :]), np.array(pos[1, :]), linewidth=0.1)
-
-    plt.figure()
-    for contact in contacts_name:
-        FK = cs.Function.deserialize(kindyn.fk(contact))
-        pos = FK(q=solution['q'])['ee_pos']
-
-        plt.title(f'plane_xz')
-        plt.scatter(np.array(pos[0, :]), np.array(pos[2, :]), linewidth=0.1)
-
-    plt.show()
-# ======================================================
-
-contact_map = dict(zip(contacts_name, [solution['f0'], solution['f1'], solution['f2'], solution['f3']]))
-
 # resampling
-resampling = False
+contact_map = dict(zip(contacts_name, [solution['f0'], solution['f1'], solution['f2'], solution['f3']]))
+resampling = args.resample
 if resampling:
 
     if isinstance(dt, cs.SX):
@@ -304,13 +228,13 @@ if resampling:
     q_res, qdot_res, qddot_res, contact_map_res, tau_res = resampler_trajectory.resample_torques(
         solution["q"], solution["q_dot"], solution["q_ddot"], dt_before_res, dt_res, dae, contact_map,
         kindyn,
-        cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED)
+        pin_ref_frame)
 
-    repl = replay_trajectory(dt_res, joint_names, q_res, contact_map_res, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED, kindyn)
+    repl = replay_trajectory(dt_res, joint_names, q_res, contact_map_res, pin_ref_frame, kindyn)
 else:
     # remember to run a robot_state_publisher
-    repl = replay_trajectory(dt, joint_names, solution['q'], contact_map, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED, kindyn)
+    repl = replay_trajectory(dt, joint_names, solution['q'], contact_map, pin_ref_frame, kindyn)
 
-repl.sleep(1.)
+print('view animated solution with roslaunch horizon_examples spot.launch!')
 repl.replay(is_floating_base=True)
 
